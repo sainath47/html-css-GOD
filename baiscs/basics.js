76e042a1936a65950b52003e4c96601f031f6f8d
Sainath47 <reddysainath47@gmail.com>
2024-07-26 18:22:58 +0530

    real deal

---
+++ baiscs/basics.js
@@ -0,0 +1,12 @@
+/**
+////////////margin: 0 auto//////////
+to center the element from left & right 
+-- flex box included 
+
+/////////margin-collapsing/////////
+- 2 paragraph elements when applied with margin on top & bottom with 20px 
+
+-so they should have been seperated by 40px right but they are seperaated by only 20px(they dont add up,they collapse)
+
+
+ */
